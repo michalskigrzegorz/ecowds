9f11ec4578f4bcbf4af5b999f635db4f7438847c
GM pulsar04 <gm@invert8.pl>
2023-04-14 16:32:14 +0200

    Added html and wp version

---
+++ ecowds-wp/footer.php
@@ -0,0 +1,45 @@
+  <footer>
+    <div class="ecowds-footer">
+      <div>
+        <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/eco-windows-logo-colors.png" width="200"/>          
+      </div>
+      <div>
+        <ul>
+          <li>
+            <svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 96 960 960" width="48"><path d="M222 976q-43.75 0-74.375-30.625T117 871V746h127V176l59.8 60 59.8-60 59.8 60 59.8-60 59.8 60 60-60 60 60 60-60 60 60 60-60v695q0 43.75-30.625 74.375T738 976H222Zm516-60q20 0 32.5-12.5T783 871V276H304v470h389v125q0 20 12.5 32.5T738 916ZM357 434v-60h240v60H357Zm0 134v-60h240v60H357Zm333-134q-12 0-21-9t-9-21q0-12 9-21t21-9q12 0 21 9t9 21q0 12-9 21t-21 9Zm0 129q-12 0-21-9t-9-21q0-12 9-21t21-9q12 0 21 9t9 21q0 12-9 21t-21 9ZM221 916h412V806H177v65q0 20 12.65 32.5T221 916Zm-44 0V806v110Z"/></svg>
+            <a href="#">Terms and conditions</a>
+          </li>
+          <li>
+            <svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 96 960 960" width="48"><path d="M480 976q-140-35-230-162.5T160 534V296l320-120 320 120v238q0 152-90 279.5T480 976Zm0-62q106-35 175.5-128.5T737 576H480V241l-260 97v196q0 12 .5 20.5T223 576h257v338Z"/></svg>
+            <a href="#">Privacy</a>              
+          </li>
+          <li>
+            <svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 96 960 960" width="48"><path d="M480 996q-63 0-106.5-43.5T330 846q0-55 34.5-95.5T450 699v-93H210V456H100V176h280v280H270v90h420v-93q-51-11-85.5-51.5T570 306q0-63 43.5-106.5T720 156q63 0 106.5 43.5T870 306q0 55-34.5 95.5T750 453v153H510v93q51 11 85.5 51.5T630 846q0 63-43.5 106.5T480 996Zm239.798-600Q757 396 783.5 369.702t26.5-63.5Q810 269 783.702 242.5t-63.5-26.5Q683 216 656.5 242.298t-26.5 63.5Q630 343 656.298 369.5t63.5 26.5ZM160 396h160V236H160v160Zm319.798 540Q517 936 543.5 909.702t26.5-63.5Q570 809 543.702 782.5t-63.5-26.5Q443 756 416.5 782.298t-26.5 63.5Q390 883 416.298 909.5t63.5 26.5ZM240 316Zm480-10ZM480 846Z"/></svg>              
+            <a class="align-content: center;" href="#">Sitemap</a>              
+          </li>                        
+        </ul>
+      </div>
+      <div class="ecowds-footer-social">
+        <ul>
+          <li>
+            <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/Facebook_f_logo_(2021).svg" width="38" />
+          </li>
+          <li>
+            <a href="">
+              <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/img/502px-Logo_of_YouTube_(2015-2017).webp" width="90"/>
+            </a>
+          </li>            
+        </ul>
+      </div>                
+    </div>
+    <div class="ecowds-footer-signature">
+      <p>
+        © 2023 ECO WINDOWS powered by ⚡ invert8
+      </p>
+    </div>
+  </footer>    
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
